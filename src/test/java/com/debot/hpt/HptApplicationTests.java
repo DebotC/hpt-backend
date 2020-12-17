577515415856ef137c53fee5da6d75fe5e8f7655
DebotC <cedric.debot@gmail.com>
2020-12-18 00:09:26 +0100

    Init

---
+++ src/test/java/com/debot/hpt/HptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debot.hpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
